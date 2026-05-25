7b74634468803cf2973f46401cf1965013ef21fa
Hariharasudhan <sudanayyappan_bcs28@mepcoeng.ac.in>
2026-05-25 22:48:05 +0530

    Add files via upload

---
+++ apps/desktop/out/preload/index.mjs
@@ -0,0 +1,9 @@
+import { contextBridge } from "electron";
+contextBridge.exposeInMainWorld("coral", {
+  platform: process.platform,
+  versions: {
+    electron: process.versions.electron,
+    chrome: process.versions.chrome,
+    node: process.versions.node
+  }
+});
